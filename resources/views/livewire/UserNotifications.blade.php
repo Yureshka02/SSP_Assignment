8fc7b9cb862ae4bf30662a3d91f6657daee01e97
WATUTHANTHIRIGE LASHANE YURESHKA ALWIS <133086262+Yureshka02@users.noreply.github.com>
2024-09-28 15:07:53 +0530

    notification functions implemented and created the routes and the views to handle the functions
    (cherry picked from commit 02f39e8d746d917a3699d31397ce37451524b877)

--- resources/views/livewire/UserNotifications.blade.php
+++ resources/views/livewire/UserNotifications.blade.php
@@ -38,6 +38,7 @@ $markAsRead = function ($notification_id) {
 
 <div x-data="{
     init(){
+        // Enable pusher logging - don't include this in production
         Pusher.logToConsole = true;
 
         var pusher = new Pusher('50a2e6693f94dde830f7', {
@@ -53,47 +54,46 @@ $markAsRead = function ($notification_id) {
     <div class="ms-3 relative">
         <x-dropdown align="right" width="48">
             <x-slot name="trigger">
-            <span class="inline-flex rounded-md">
-                <button type="button"
-                        class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 transition ease-in-out duration-150">
-                    Notifications ({{ $notifications->count() }})
-                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                         stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                    </svg>
-                </button>
-            </span>
+                            <span class="inline-flex rounded-md">
+                                    <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                        Notifications ({{ $notifications->count() }})
+
+                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                             viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                                        </svg>
+                                    </button>
+                                </span>
             </x-slot>
 
             <x-slot name="content">
                 <!-- Account Management -->
-                <div class="block px-4 py-2 text-xs font-semibold text-gray-600 border-b border-gray-200">
+                <div class="block px-4 py-2 text-xs text-gray-400">
                     {{ __('Notifications') }}
                 </div>
 
-                <ul class="max-h-60 overflow-auto divide-y divide-gray-200">
+                <ul>
                     @foreach($notifications as $notification)
-                        <li class="{{ !$notification->read_at ? 'bg-red-100' : '' }} flex justify-between items-center p-4 hover:bg-gray-50 transition duration-150">
+                        <li class="{{ !$notification->read_at ? 'bg-[#FF2D20]/10' : '' }} flex space-x-3">
                             <a href="{{ $notification->data['action'] }}"
-                               class="flex items-center space-x-3 text-sm text-gray-700 hover:text-gray-900 w-full">
+                               class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex">
 
                                 @if($notification->data['icon'] == 'info')
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                         stroke="currentColor" class="w-6 h-6 text-blue-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5"
+                                         stroke="currentColor" class="size-6">
                                         <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
+                                              d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"/>
                                     </svg>
                                 @endif
 
-                                <span>{{ $notification->data['message'] }}</span>
+
+                                {{ $notification->data['message'] }}
                             </a>
-                            <button type="button" wire:click="markAsRead('{{ $notification->id }}')"
-                                    class="text-gray-400 hover:text-gray-600 transition duration-150 focus:outline-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                     stroke="currentColor" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
+
+                            <button type="button" wire:click="markAsRead('{{ $notification->id }}')">X</button>
                         </li>
                     @endforeach
                 </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,16 @@
 <?php
 
-use App\Enums\Role;
-use App\Http\Controllers\AppointmentController;
+
 use App\Http\Controllers\BookingsController;
 use App\Http\Controllers\CacheController;
 use App\Http\Controllers\CustomersController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FacilitiesController;
-use App\Http\Controllers\GarageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\HomeeController;
 use App\Http\Controllers\LogController;
 use App\Http\Controllers\NotificationController;
-use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\serviceHomeController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\SuppliersController;
-use App\Models\Review;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 
@@ -28,106 +22,117 @@ Route::middleware([
     'verified',
 ])->group(function () {
 
-    //routes for services and products
     Route::get('/products-home', [HomeeController::class, '__invoke'])->name('products-home');
-    Route::get('/services-home', [serviceHomeController::class, '__invoke'])->name('services-home');
 
-    //garage routes
-    Route::get('garage-check-out', [
-        GarageController::class,
-        'garage_checkOut'
-    ])->name('garage-checkout');
 
-    Route::post('garage-check-out', [
-        GarageController::class,
-        'completeGarage'
-    ])->name('garage.complete');
+Route::get('check-out', [
+    CartController::class,
+    'checkOut'
+])->name('checkout');
 
-    Route::get('thank-you/{appointment}', [
-        GarageController::class,
-        'appointmentConfirmation'
-    ])->name('garage.confirm');
+Route::post('check-out', [
+    CartController::class,
+    'completeOrder'
+])->name('checkout.complete');
 
-    // cart routes
-    Route::get('check-out', [
-        CartController::class,
-        'checkOut'
-    ])->name('checkout');
-
-    Route::post('check-out', [
-        CartController::class,
-        'completeOrder'
-    ])->name('checkout.complete');
-
-    Route::get('thank-you/{order}', [
-        CartController::class,
-        'orderConfirmation'
-    ])->name('checkout.confirm');
+Route::get('thank-you/{order}', [
+    CartController::class,
+    'orderConfirmation'
+])->name('checkout.confirm');
 
 
 });
 
-
-
-
-Route::get('/cache-test', [
+Route::get('/cache-test',[
     CacheController::class,
     'index'
 ]);
 
-Route::get('/logs', [
+Route::get('/logs',[
     LogController::class,
     'index'
 ]);
 
-Route::get('/session', [
+Route::get('/session',[
     SessionController::class,
     'index'
 ]);
 
-Route::get('/notification', [
+Route::get('/notification',[
     NotificationController::class,
     'index'
 ]);
 
-//home route
-Route::get('/', HomeController::class)
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('/',HomeController::class)
     ->name('home');
 
 
-//reviews routes
-Route::middleware('auth')->group(function () {
-    Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
+Route::middleware([
+    'role:Admin,Customer',
+])->get('/dev', function () {
+    dd(auth()->user()->role == App\Enums\Role::Customer);
 });
 
-Route::get('/reviews', [ReviewController::class, 'index'])->name('reviews.index');
 
 
-Route::get('/', function () {
-    $reviews = Review::with('user')->latest()->get(); // Fetch reviews
-    return view('home', compact('reviews')); // Pass reviews to the view
-})->name('home');
 
-//dashboard route
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->name('dashboard')
-    ->middleware(['auth', 'verified','role:Admin' ]);
+
+
+
+Route::get('/',HomeController::class)
+    ->name('home');
+
+
+Route::get('/reservations', function () {
+    return view('dashboard');
+})  ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
 
 //bookings route
-Route::resource('Bookings', BookingsController::class)
+Route::resource('Bookings',BookingsController::class)
     ->middleware(['auth', 'verified']);
 
 //facilities route
-Route::resource('facilities', FacilitiesController::class)
+Route::resource('facilities',FacilitiesController::class)
     ->middleware(['auth', 'verified']);
-
 //suppliers route
-Route::resource('suppliers', SuppliersController::class)
+Route::resource('suppliers',SuppliersController::class)
     ->middleware(['auth', 'verified']);
-
 //customers route
-Route::resource('customers', CustomersController::class)
+Route::resource('customers',CustomersController::class)
+    ->middleware(['auth', 'verified']);
+//dashboard route
+Route::resource('dashboard',HomeController::class)
     ->middleware(['auth', 'verified']);
 
-//appointments route
-Route::post('/appointments/notify', [AppointmentController::class, 'notify']);
+
+
+
+
+
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
